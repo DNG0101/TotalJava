3d73b4bdd20062505ee2c1146485b0cbdf41e84f
Donthula Nithish <98210183+DNG0101@users.noreply.github.com>
2026-02-23 15:43:26 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,38 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
 
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
 
-        int a=sc.nextInt();
-        int b=sc.nextInt();
+        int a = 5;   // default value
+        int b = 3;   // default value
 
-        System.out.println("Addition: "+(a+b));
-        System.out.println("Subtraction: "+(a-b));
-        System.out.println("Multiplication: "+(a*b));
-        System.out.println("Division: "+(a/b));
+        File file = new File("input.txt");
+
+        if (file.exists()) {
+
+            Scanner sc = new Scanner(file);
+
+            if (sc.hasNextInt()) {
+                a = sc.nextInt();
+            }
+
+            if (sc.hasNextInt()) {
+                b = sc.nextInt();
+            }
+
+            sc.close();
+        }
+
+        System.out.println("======= JAVA CLOUD PROGRAM =======");
+        System.out.println("First number  : " + a);
+        System.out.println("Second number : " + b);
+        System.out.println("----------------------------------");
+
+        System.out.println("Addition       : " + Calculator.add(a, b));
+        System.out.println("Subtraction    : " + Calculator.sub(a, b));
+        System.out.println("Multiplication : " + Calculator.mul(a, b));
+        System.out.println("Division       : " + Calculator.div(a, b));
     }
 }
