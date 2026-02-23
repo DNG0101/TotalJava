d5c468ab83a891c475edae249fac40110c50fdae
Donthula Nithish <98210183+DNG0101@users.noreply.github.com>
2026-02-23 15:05:40 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        int a = 5;
+        int b = 3;
+
         File file = new File("input.txt");
 
-        int a = 0;
-        int b = 0;
-
+        // Only read if numbers exist
         if (file.exists()) {
             Scanner sc = new Scanner(file);
-            a = sc.nextInt();
-            b = sc.nextInt();
+
+            if (sc.hasNextInt()) {
+                a = sc.nextInt();
+            }
+
+            if (sc.hasNextInt()) {
+                b = sc.nextInt();
+            }
+
             sc.close();
         }
 
         System.out.println("======= JAVA CLOUD PROGRAM =======");
         System.out.println("First number  : " + a);
         System.out.println("Second number : " + b);
-
         System.out.println("----------------------------------");
+
         System.out.println("Addition       : " + Calculator.add(a, b));
         System.out.println("Subtraction    : " + Calculator.sub(a, b));
         System.out.println("Multiplication : " + Calculator.mul(a, b));
